e8fe4bbe6e16ddf1350d733df0dba61358cbfec2
bcoe <bencoe@google.com>
2019-05-08 22:20:13 -0700

    fix: shebang must be first line

--- .github/action/mint-release/entrypoint.sh
+++ .github/action/mint-release/entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Copyright 2019 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh
 
 set -e
 
