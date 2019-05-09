ac06e1c0c9b8a61d5a52ffe3886396b64eb9b6ed
Benjamin Coe <bencoe@google.com>
2019-05-09 15:54:23 -0700

    feat: releases can only be managed by users within ORGS_ACL

--- .github/action/mint-release/check.js
+++ .github/action/mint-release/check.js
@@ -19,13 +19,35 @@
 // made by user with write access to repo.
 // TODO: check that user has write access.
 
-const partialIssueTitle = 'proposal for next release';
 const event = require(process.env.GITHUB_EVENT_PATH);
-console.info(JSON.stringify(event, null, 2));
+const fetch = require('node-fetch');
 
-if (issue.title.indexOf(partialIssueTitle) !== -1) {
-  process.exit(0);
+// only users in ORGS_ACL may create releases.
+const ORGS_ACL = process.env.ORGS_ACL.split(',')
+
+// releases can only be created if issue has PARTIALS_ISSUE_TITLE.
+const PARTIALS_ISSUE_TITLE = 'proposal for next release';
+
+if (event.action === 'created' && event.issue.title.indexOf(PARTIALS_ISSUE_TITLE) !== -1) {
+  console.info(`comment created on issue containing: ${PARTIALS_ISSUE_TITLE}`);
+  // ensure the user was in one of our supported organizations:
+  fetch(event.sender.organizations_url)
+    .then(res => res.json())
+    .then(orgs => {
+      for (let i = 0, org; (org = orgs[i]) !== undefined; i++) {
+        if (ORGS_ACL.indexOf(org.login) !== -1) {
+          console.info(`${process.env.GITHUB_ACTOR} was in ${org.login} creating release`);
+          process.exit(0);
+        }
+      }
+      console.error(`${process.env.GITHUB_ACTOR} not found in ${ORGS_ACL}`);
+      process.exit(1);
+    })
+    .catch((err) => {
+      console.error(err.stack);
+      process.exit(1);
+    });
 } else {
-  console.info(`issue title did not contain ${partialIssueTitle}`)
+  console.info(`issue title did not contain: ${partialIssueTitle}`)
   process.exit(1);
 }
