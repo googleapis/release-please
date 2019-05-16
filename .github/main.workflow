edd551843381eccd559c7fc5d0d7b2a6f2455e38
Benjamin Coe <bencoe@google.com>
2019-05-15 17:25:16 -0700

    update github action to reflect new detect-checked functionality

--- .github/main.workflow
+++ .github/main.workflow
@@ -1,5 +1,5 @@
 workflow "Candidate Issue" {
-  on = "schedule(*/5 * * * *)"
+  on = "schedule(*/8 * * * *)"
   resolves = ["candidate-issue"]
 }
 
@@ -8,6 +8,20 @@ action "candidate-issue" {
   env = {
     PACKAGE_NAME = "release-please"
     RELEASE_PLEASE_COMMAND = "candidate-issue"
+  }
+  secrets = ["GITHUB_TOKEN"]
+}
+
+workflow "Detect Checked" {
+  on = "schedule(*/4 * * * *)"
+  resolves = ["detect-checked"]
+}
+
+action "detect-checked" {
+  uses = "googleapis/release-please/.github/action/release-please@master"
+  env = {
+    PACKAGE_NAME = "release-please"
+    RELEASE_PLEASE_COMMAND = "detect-checked"
   }
   secrets = ["GITHUB_TOKEN"]
 }
--- src/bin/release-please.ts
+++ src/bin/release-please.ts
@@ -141,7 +141,7 @@ yargs
           console.info(chalk.green(
               '----- put the content below in .github/main.workflow -----'));
           console.info(`workflow "Candidate Issue" {
-  on = "schedule(*/5 * * * *)"
+  on = "schedule(*/8 * * * *)"
   resolves = ["candidate-issue"]
 }
 
@@ -150,6 +150,20 @@ action "candidate-issue" {
   env = {
     PACKAGE_NAME = "${argv.packageName}"
     RELEASE_PLEASE_COMMAND = "candidate-issue"
+  }
+  secrets = ["GITHUB_TOKEN"]
+}
+
+workflow "Detect Checked" {
+  on = "schedule(*/4 * * * *)"
+  resolves = ["detect-checked"]
+}
+
+action "detect-checked" {
+  uses = "googleapis/release-please/.github/action/release-please@master"
+  env = {
+    PACKAGE_NAME = "${argv.packageName}"
+    RELEASE_PLEASE_COMMAND = "detect-checked"
   }
   secrets = ["GITHUB_TOKEN"]
 }
