21c098cd7a5188ab105a5cfbc8b1a3e48576b368
bcoe <bencoe@google.com>
2019-05-26 14:27:18 -0700

    nit: slight spacing fix

--- src/github.ts
+++ src/github.ts
@@ -124,7 +124,7 @@ export class GitHub {
                             }
                           }
                         }
-                    	}
+                      }
                     }
                   }
                 }
