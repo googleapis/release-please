73b77d0a1908e17d773054bafe50cb58e2b399f9
Benjamin Coe <bencoe@google.com>
2019-06-07 11:53:47 -0700

    feat!: update GitHub action to run on PR rather than commit

--- .github/action/release-please/entrypoint.sh
+++ .github/action/release-please/entrypoint.sh
@@ -15,8 +15,12 @@
 
 set -e
 
-export COMMAND=${RELEASE_PLEASE_COMMAND:-candidate-issue}
+export COMMAND=${RELEASE_PLEASE_COMMAND:-release-pr}
+export ACTION==$(json "$GITHUB_EVENT_PATH" action)
+export MERGED=$(json "$GITHUB_EVENT_PATH" pull_reuest.merged)
 
-release-please $COMMAND --token=$GITHUB_TOKEN \
-  --repo-url="git@github.com:$GITHUB_REPOSITORY.git" \
-  --package-name=$PACKAGE_NAME
+if [[ "$ACTION" = "closed" ]] && [[ "$MERGED" == "true" ]]; then
+  release-please $COMMAND --token=$GITHUB_TOKEN \
+    --repo-url="git@github.com:$GITHUB_REPOSITORY.git" \
+    --package-name=$PACKAGE_NAME
+fi
