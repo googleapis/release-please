fe1198fbf039ad78b69c7177958464974b1b1ec3
Jeff Ching <chingor@google.com>
2019-10-08 16:42:34 -0700

    fix(java): snapshot bumps should not autorelease

--- test/releasers/java-yoshi.ts
+++ test/releasers/java-yoshi.ts
@@ -167,12 +167,129 @@ describe('JavaYoshi', () => {
         }
       )
       .reply(200, { number: 1 })
+      .post(
+        '/repos/googleapis/java-trace/issues/1/labels',
+        (req: { [key: string]: string }) => {
+          snapshot('labels', req);
+          return true;
+        }
+      )
+      .reply(200, {})
       // this step tries to close any existing PRs; just return an empty list.
       .get('/repos/googleapis/java-trace/pulls?state=open&per_page=100')
       .reply(200, []);
     const releasePR = new JavaYoshi({
       repoUrl: 'googleapis/java-trace',
-      label: 'autorelease: pending',
+      releaseType: ReleaseType.JavaYoshi,
+      // not actually used by this type of repo.
+      packageName: 'java-trace',
+      apiUrl: 'https://api.github.com',
+    });
+    await releasePR.run();
+    req.done();
+  });
+  it('creates a snapshot PR', async () => {
+    const versionsContent = readFileSync(
+      resolve(fixturesPath, 'released-versions.txt'),
+      'utf8'
+    );
+    const readmeContent = readFileSync(
+      resolve(fixturesPath, 'README.md'),
+      'utf8'
+    );
+    const pomContents = readFileSync(resolve(fixturesPath, 'pom.xml'), 'utf8');
+    const graphql = JSON.parse(
+      readFileSync(resolve(fixturesPath, 'commits-yoshi-java.json'), 'utf8')
+    );
+    const req = nock('https://api.github.com')
+      .get('/repos/googleapis/java-trace/pulls?state=closed&per_page=100')
+      .reply(200, undefined)
+      .get('/repos/googleapis/java-trace/contents/versions.txt')
+      .reply(200, {
+        content: Buffer.from(versionsContent, 'utf8').toString('base64'),
+        sha: 'abc123',
+      })
+      // fetch semver tags, this will be used to determine
+      // the delta since the last release.
+      .get('/repos/googleapis/java-trace/tags?per_page=100')
+      .reply(200, [
+        {
+          name: 'v0.20.3',
+          commit: {
+            sha: 'da6e52d956c1e35d19e75e0f2fdba439739ba364',
+          },
+        },
+      ])
+      // finding pom.xml files
+      .get('/search/code?q=filename%3Apom.xml+repo%3Agoogleapis%2Fjava-trace')
+      .reply(200, {
+        total_count: 1,
+        items: [{ name: 'pom.xml', path: 'pom.xml' }],
+      })
+      // getting the latest tag
+      .get('/repos/googleapis/java-trace/git/refs?per_page=100')
+      .reply(200, [{ ref: 'refs/tags/v0.20.3' }])
+      // creating a new branch
+      .post('/repos/googleapis/java-trace/git/refs')
+      .reply(200)
+      // update versions.txt
+      .get(
+        '/repos/googleapis/java-trace/contents/versions.txt?ref=refs%2Fheads%2Frelease-v0.20.4-SNAPSHOT'
+      )
+      .reply(200, {
+        content: Buffer.from(versionsContent, 'utf8').toString('base64'),
+        sha: 'abc123',
+      })
+      .put(
+        '/repos/googleapis/java-trace/contents/versions.txt',
+        (req: { [key: string]: string }) => {
+          snapshot(
+            'versions-snapshot',
+            Buffer.from(req.content, 'base64').toString('utf8')
+          );
+          return true;
+        }
+      )
+      .reply(200)
+      // update pom.xml
+      .get(
+        '/repos/googleapis/java-trace/contents/pom.xml?ref=refs%2Fheads%2Frelease-v0.20.4-SNAPSHOT'
+      )
+      .reply(200, {
+        content: Buffer.from(pomContents, 'utf8').toString('base64'),
+        sha: 'abc123',
+      })
+      .put(
+        '/repos/googleapis/java-trace/contents/pom.xml',
+        (req: { [key: string]: string }) => {
+          snapshot('pom-snapshot', Buffer.from(req.content, 'base64').toString('utf8'));
+          return true;
+        }
+      )
+      .reply(200)
+      // create release
+      .post(
+        '/repos/googleapis/java-trace/pulls',
+        (req: { [key: string]: string }) => {
+          const body = req.body.replace(/\([0-9]{4}-[0-9]{2}-[0-9]{2}\)/g, '');
+          snapshot('PR body-snapshot', body);
+          return true;
+        }
+      )
+      .reply(200, { number: 1 })
+      .post(
+        '/repos/googleapis/java-trace/issues/1/labels',
+        (req: { [key: string]: string }) => {
+          snapshot('labels-snapshot', req);
+          return true;
+        }
+      )
+      .reply(200, {})
+      // this step tries to close any existing PRs; just return an empty list.
+      .get('/repos/googleapis/java-trace/pulls?state=open&per_page=100')
+      .reply(200, []);
+    const releasePR = new JavaYoshi({
+      repoUrl: 'googleapis/java-trace',
       releaseType: ReleaseType.JavaYoshi,
       // not actually used by this type of repo.
       packageName: 'java-trace',
