6664d16cb1d176bba02953f10c0fe1e999f8a5ac
Benjamin Coe <bencoe@google.com>
2019-10-07 17:51:08 -0700

    fix: the correct API was not being used for iterating map

--- src/updaters/root-composer.ts
+++ src/updaters/root-composer.ts
@@ -44,17 +44,16 @@ export class RootComposer implements Update {
       return content;
     }
     const parsed = JSON.parse(content);
-    Object.keys(this.versions).forEach((key: string) => {
-      if (this.versions) {
-        const version: string = this.versions.get(key) || '1.0.0';
+    if (this.versions) {
+      for (let [key, version] of this.versions.entries()) {
+        version = version || '1.0.0';
         checkpoint(
           `updating ${key} from ${parsed.replace[key]} to ${version}`,
           CheckpointType.Success
         );
         parsed.replace[key] = version;
       }
-    });
-
+    }
     return JSON.stringify(parsed, null, 4) + '\n';
   }
 }
