670d872bebdb72fc6bd490caefdd3210a39ef64e
Benjamin E. Coe <bencoe@google.com>
2020-04-03 18:09:42 -0700

    feat: handle multiline bulleted commit messages (#396)

--- __snapshots__/indent-commit.js
+++ __snapshots__/indent-commit.js
@@ -9,3 +9,12 @@ feat: my awesome commit message
   * testing one line
   * testing second line
 `;
+
+exports['indentCommit handles multiple lines of multi-line text 1'] = `
+feat: my awesome commit message
+  * testing one line
+    this is a second line of text
+    this is a third line
+  * testing second line
+    this is a second line
+`;
