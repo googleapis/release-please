83e8165b3dd78459a97c444878e69705e66bdcb8
Alexander Fenster <fenster@google.com>
2020-05-18 18:15:09 -0700

    feat: simple version.txt releaser (#436)

---
+++ __snapshots__/simple.js
@@ -0,0 +1,39 @@
+exports['CHANGELOG-simple'] = `
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/simple-test-repo/compare/v0.123.4...v0.123.5) 
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/simple-test-repo/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/simple-test-repo/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+`
+
+exports['version-txt-simple'] = `
+0.123.5
+
+`
+
+exports['PR body-simple'] = `
+:robot: I have created a release \\*beep\\* \\*boop\\* 
+---
+### [0.123.5](https://www.github.com/googleapis/simple-test-repo/compare/v0.123.4...v0.123.5) 
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/simple-test-repo/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/simple-test-repo/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+---
+
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please).
+`
+
+exports['labels-simple'] = {
+  'labels': [
+    'autorelease: pending'
+  ]
+}
