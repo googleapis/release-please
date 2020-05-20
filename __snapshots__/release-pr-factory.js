fcc1cf1d23532855c18933a973dabd5a7cdbeea4
bcoe <bencoe@google.com>
2020-05-19 17:51:06 -0700

    feat: introduce static factory helper

---
+++ __snapshots__/release-pr-factory.js
@@ -0,0 +1,79 @@
+exports['ReleasePRFactory build returns instance of dynamically loaded releaser 1'] = `
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/simple-test-repo/compare/v0.123.4...v0.123.5) 
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/simple-test-repo/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/simple-test-repo/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+`
+
+exports['ReleasePRFactory build returns instance of dynamically loaded releaser 2'] = `
+0.123.5
+
+`
+
+exports['ReleasePRFactory build returns instance of dynamically loaded releaser 3'] = `
+:robot: I have created a release \\*beep\\* \\*boop\\* 
+---
+### [0.123.5](https://www.github.com/googleapis/simple-test-repo/compare/v0.123.4...v0.123.5) 
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/simple-test-repo/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/simple-test-repo/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+---
+
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please).
+`
+
+exports['ReleasePRFactory build returns instance of dynamically loaded releaser 4'] = {
+  "labels": [
+    "autorelease: pending"
+  ]
+}
+
+exports['ReleasePRFactory buildStatic returns an instance of a statically loaded releaser 1'] = `
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/simple-test-repo/compare/v0.123.4...v0.123.5) 
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/simple-test-repo/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/simple-test-repo/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+`
+
+exports['ReleasePRFactory buildStatic returns an instance of a statically loaded releaser 2'] = `
+0.123.5
+
+`
+
+exports['ReleasePRFactory buildStatic returns an instance of a statically loaded releaser 3'] = `
+:robot: I have created a release \\*beep\\* \\*boop\\* 
+---
+### [0.123.5](https://www.github.com/googleapis/simple-test-repo/compare/v0.123.4...v0.123.5) 
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/simple-test-repo/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/simple-test-repo/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+---
+
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please).
+`
+
+exports['ReleasePRFactory buildStatic returns an instance of a statically loaded releaser 4'] = {
+  "labels": [
+    "autorelease: pending"
+  ]
+}
