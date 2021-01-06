9d40c3adf378c05a3cae411c49f4a11e89717bc9
Joe Bottigliero <joe@bottigliero.com>
2021-01-05 19:29:57 -0600

    test: adds simple tests for github-release CLI and --debug flag (additional command.ts coverage)

--- test/cli.ts
+++ test/cli.ts
@@ -14,8 +14,9 @@
 
 import {resolve} from 'path';
 
-import {describe, it} from 'mocha';
+import {describe, it, afterEach} from 'mocha';
 import {expect, assert} from 'chai';
+import * as sinon from 'sinon';
 
 import command from '../src/bin/command';
 
@@ -23,7 +24,12 @@ function getExampleConfigurationPath() {
   return resolve('./test/fixtures/config', 'simple.json');
 }
 
+const sandbox = sinon.createSandbox();
+
 describe('CLI', () => {
+  afterEach(() => {
+    sandbox.restore();
+  });
   describe('release-pr', () => {
     it('can be configured using flags', () => {
       const argv = command.parse(
@@ -62,6 +68,44 @@ describe('CLI', () => {
         JSON.parse(argv.changelogTypes as string),
         argv.changelogSections as []
       );
+    });
+
+    it('supports --debug', async () => {
+      const messages: string[] = [];
+      sandbox.replace(console, 'error', (...args: string[]) => {
+        messages.push(...args);
+      });
+      await new Promise(resolve => {
+        command
+          /**
+           * This could/should eventually be replaced with async handling in `yargs.parse`
+           * @see https://github.com/yargs/yargs/issues/1069
+           */
+          .onFinishCommand(() => {
+            resolve(22);
+          })
+          .parse(
+            'release-pr --debug --repo-url=googleapis/release-please-cli --package-name=cli-package'
+          );
+      });
+      expect(messages[0]).to.match(/command release-pr failed with status 500/);
+      // we know it's the verbose logging if it has the stack seperator
+      expect(messages[1]).to.eq('---------');
+      // includes the stack
+      expect(messages[2]).to.match(/HttpError/);
+    });
+  });
+
+  describe('github-release', () => {
+    it('can be configured using flags', () => {
+      const argv = command.parse(
+        'github-release --repo-url=googleapis/release-please-cli --package-name=cli-package'
+      );
+      expect(argv).includes({
+        repoUrl: 'googleapis/release-please-cli',
+        releaseType: 'node',
+        packageName: 'cli-package',
+      });
     });
   });
 });
