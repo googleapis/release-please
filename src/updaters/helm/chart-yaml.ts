160011523897684ff5651851bf4ade7365d7fac6
Abhinav Khanna <abhinav.khanna@salesforce.com>
2021-02-05 12:45:57 -0600

    fix: linting issues

--- src/updaters/helm/chart-yaml.ts
+++ src/updaters/helm/chart-yaml.ts
@@ -4,29 +4,29 @@ import {GitHubFileContents} from '../../github';
 import * as yaml from 'yaml';
 
 export class ChartYaml implements Update {
-    path: string;
-    changelogEntry: string;
-    version: string;
-    versions?: VersionsMap;
-    packageName: string;
-    create: boolean;
-    contents?: GitHubFileContents;
+  path: string;
+  changelogEntry: string;
+  version: string;
+  versions?: VersionsMap;
+  packageName: string;
+  create: boolean;
+  contents?: GitHubFileContents;
 
-    constructor(options: UpdateOptions) {
-        this.create = false;
-        this.path = options.path;
-        this.changelogEntry = options.changelogEntry;
-        this.version = options.version;
-        this.packageName = options.packageName;
-    }
+  constructor(options: UpdateOptions) {
+    this.create = false;
+    this.path = options.path;
+    this.changelogEntry = options.changelogEntry;
+    this.version = options.version;
+    this.packageName = options.packageName;
+  }
 
-    updateContent(content: string): string {
-        const parsed = yaml.parse(content);
-        checkpoint(
-            `updating ${this.path} from ${parsed.version} to ${this.version}`,
-            CheckpointType.Success
-        );
-        parsed.version = this.version;
-        return yaml.stringify(parsed) + '\n';
-    }
+  updateContent(content: string): string {
+    const parsed = yaml.parse(content);
+    checkpoint(
+      `updating ${this.path} from ${parsed.version} to ${this.version}`,
+      CheckpointType.Success
+    );
+    parsed.version = this.version;
+    return yaml.stringify(parsed) + '\n';
+  }
 }
