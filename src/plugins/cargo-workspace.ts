db7b56ea2d551be304d9bfb0502c8f34552ec903
Amos Wenger <amoswenger@gmail.com>
2021-07-12 13:30:58 +0200

    Update changelog when bumping crates

--- src/plugins/cargo-workspace.ts
+++ src/plugins/cargo-workspace.ts
@@ -12,13 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {FileData} from 'code-suggester/build/src/types';
+import {Changes, FileData} from 'code-suggester/build/src/types';
 import * as semver from 'semver';
-import {ManifestPackageWithPRData} from '..';
+import {ManifestPackage, ManifestPackageWithPRData} from '..';
+import {ConventionalCommits} from '../conventional-commits';
 import {GitHubFileContents} from '../github';
+import {Changelog} from '../updaters/changelog';
 import {CargoLock} from '../updaters/rust/cargo-lock';
 import {CargoToml} from '../updaters/rust/cargo-toml';
-import {CargoManifest, parseCargoManifest} from '../updaters/rust/common';
+import {
+  CargoDependencies,
+  CargoDependency,
+  CargoManifest,
+  parseCargoManifest,
+} from '../updaters/rust/common';
 import {VersionsMap} from '../updaters/update';
 import {CheckpointType} from '../util/checkpoint';
 import {ManifestPlugin} from './plugin';
@@ -132,6 +139,13 @@ export default class CargoWorkspaceDependencyUpdates extends ManifestPlugin {
           crate.manifestPath,
           updatedManifest
         );
+
+        await this.setChangelogEntry(
+          crate.manifestPkg.config,
+          crate.manifestPkg.prData.changes,
+          crate.manifestContent,
+          updatedManifest.content! // bug if undefined
+        );
       } else {
         // package was not bumped by release-please, but let's bump it ourselves,
         // because one of its dependencies was upgraded.
@@ -158,7 +172,7 @@ export default class CargoWorkspaceDependencyUpdates extends ManifestPlugin {
         const changes = new Map([[crate.manifestPath, updatedManifest]]);
 
         newManifestVersions.set(crate.path, version);
-        pkgsWithPRData.push({
+        const manifestPkg: ManifestPackageWithPRData = {
           config: {
             releaseType: 'rust',
             packageName: crate.name,
@@ -168,7 +182,15 @@ export default class CargoWorkspaceDependencyUpdates extends ManifestPlugin {
             changes,
             version,
           },
-        });
+        };
+        pkgsWithPRData.push(manifestPkg);
+
+        await this.setChangelogEntry(
+          manifestPkg.config,
+          manifestPkg.prData.changes,
+          crate.manifestContent,
+          updatedManifest.content! // bug if undefined
+        );
       }
     }
 
@@ -212,6 +234,202 @@ export default class CargoWorkspaceDependencyUpdates extends ManifestPlugin {
     }
 
     return [newManifestVersions, pkgsWithPRData];
+  }
+
+  private async setChangelogEntry(
+    config: ManifestPackage,
+    changes: Changes,
+    originalManifestContent: string,
+    updatedManifestContent: string
+  ) {
+    const originalManifest = parseCargoManifest(originalManifestContent);
+    const updatedManifest = parseCargoManifest(updatedManifestContent);
+
+    const depUpdateNotes = this.getChangelogDepsNotes(
+      originalManifest,
+      updatedManifest
+    );
+    if (!depUpdateNotes) {
+      return;
+    }
+
+    const cc = new ConventionalCommits({
+      changelogSections: [{type: 'deps', section: 'Dependencies'}],
+      commits: [
+        {
+          sha: '',
+          message: 'deps: The following workspace dependencies were updated',
+          files: [],
+        },
+      ],
+      owner: this.gh.owner,
+      repository: this.gh.repo,
+      bumpMinorPreMajor: config.bumpMinorPreMajor,
+    });
+    let tagPrefix = config.packageName;
+    tagPrefix += '-v';
+    const originalVersion = originalManifest.package?.version ?? '?';
+    const updatedVersion = updatedManifest.package?.version ?? '?';
+    let changelogEntry = await cc.generateChangelogEntry({
+      version: originalVersion,
+      currentTag: tagPrefix + originalVersion,
+      previousTag: tagPrefix + updatedVersion,
+    });
+    changelogEntry += depUpdateNotes;
+
+    let updatedChangelog;
+    let changelogPath = config.changelogPath ?? 'CHANGELOG.md';
+
+    if (config.path !== '.') {
+      changelogPath = `${config.path}/${changelogPath}`;
+    }
+    const exChangelog = changes.get(changelogPath)?.content;
+    if (exChangelog) {
+      updatedChangelog = this.updateChangelogEntry(
+        exChangelog,
+        changelogEntry,
+        updatedManifest
+      );
+    } else {
+      updatedChangelog = await this.newChangelogEntry(
+        changelogEntry,
+        changelogPath,
+        updatedManifest
+      );
+    }
+    if (updatedChangelog) {
+      changes.set(changelogPath, {
+        content: updatedChangelog,
+        mode: '100644',
+      });
+    }
+  }
+
+  private getChangelogDepsNotes(
+    originalManifest: CargoManifest,
+    updatedManifest: CargoManifest
+  ): string {
+    let depUpdateNotes = '';
+    type DT = 'dependencies' | 'dev-dependencies' | 'build-dependencies';
+    const depTypes: DT[] = [
+      'dependencies',
+      'dev-dependencies',
+      'build-dependencies',
+    ];
+    const depVer = (
+      s: string | CargoDependency | undefined
+    ): string | undefined => {
+      if (s === undefined) {
+        return undefined;
+      }
+      if (typeof s === 'string') {
+        return s;
+      } else {
+        return s.version;
+      }
+    };
+    type DepMap = {[key: string]: string};
+    const getDepMap = (cargoDeps: CargoDependencies): DepMap => {
+      const result: DepMap = {};
+      for (const [key, val] of Object.entries(cargoDeps)) {
+        const ver = depVer(val);
+        if (ver) {
+          result[key] = ver;
+        }
+      }
+      return result;
+    };
+
+    const updates: Map<DT, string[]> = new Map();
+    for (const depType of depTypes) {
+      const depUpdates = [];
+      const pkgDepTypes = updatedManifest[depType];
+      if (pkgDepTypes === undefined) {
+        continue;
+      }
+      for (const [depName, currentDepVer] of Object.entries(
+        getDepMap(pkgDepTypes)
+      )) {
+        const origDepVer = depVer(originalManifest[depType]?.[depName]);
+        if (currentDepVer !== origDepVer) {
+          depUpdates.push(
+            `\n    * ${depName} bumped from ${origDepVer} to ${currentDepVer}`
+          );
+        }
+      }
+      if (depUpdates.length > 0) {
+        updates.set(depType, depUpdates);
+      }
+    }
+    for (const [dt, notes] of updates) {
+      depUpdateNotes += `\n  * ${dt}`;
+      for (const note of notes) {
+        depUpdateNotes += note;
+      }
+    }
+    return depUpdateNotes;
+  }
+
+  private updateChangelogEntry(
+    exChangelog: string,
+    changelogEntry: string,
+    updatedManifest: CargoManifest
+  ): string {
+    const pkgVersion = updatedManifest.package?.version ?? '?';
+    const pkgName = updatedManifest.package?.name ?? '?';
+
+    changelogEntry = changelogEntry.replace(
+      new RegExp(`^###? \\[${pkgVersion}\\].*### Dependencies`, 's'),
+      '### Dependencies'
+    );
+    const match = exChangelog.match(
+      new RegExp(
+        `(?<before>^.*?###? \\[${pkgVersion}\\].*?\n)(?<after>###? [0-9[].*)`,
+        's'
+      )
+    );
+    if (!match) {
+      this.log(
+        `Appending update notes to end of changelog for ${pkgName}`,
+        CheckpointType.Failure
+      );
+      changelogEntry = `${exChangelog}\n\n\n${changelogEntry}`;
+    } else {
+      const {before, after} = match.groups!;
+      changelogEntry = `${before.trim()}\n\n\n${changelogEntry}\n\n${after.trim()}`;
+    }
+    return changelogEntry;
+  }
+
+  private async newChangelogEntry(
+    changelogEntry: string,
+    changelogPath: string,
+    updatedManifest: CargoManifest
+  ): Promise<string> {
+    let changelog;
+    try {
+      changelog = (await this.gh.getFileContents(changelogPath)).parsedContent;
+    } catch (e) {
+      if (e.status !== 404) {
+        this.log(
+          `Failed to retrieve ${changelogPath}: ${e}`,
+          CheckpointType.Failure
+        );
+        return '';
+      } else {
+        this.log(
+          `Creating a new changelog at ${changelogPath}`,
+          CheckpointType.Success
+        );
+      }
+    }
+    const changelogUpdater = new Changelog({
+      path: changelogPath,
+      changelogEntry,
+      version: updatedManifest.package?.version ?? '?',
+      packageName: updatedManifest.package?.name ?? '?',
+    });
+    return changelogUpdater.updateContent(changelog);
   }
 
   private async getAllCrateInfos(
