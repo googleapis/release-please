fa317b2ef88bd153001b85984b8f457a4d9eec1e
Jeff Ching <chingor@google.com>
2021-07-14 18:16:26 -0700

    fix(go-yoshi): Release-As should only apply to the correct file scope (#969)
    Fixes #878

---
+++ __snapshots__/go-yoshi.js
@@ -0,0 +1,149 @@
+exports['GoYoshi run creates a release PR: changes'] = `
+
+filename: CHANGES.md
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/v0.123.4...v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/google-cloud-go/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/google-cloud-go/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+`
+
+exports['GoYoshi run creates a release PR: options'] = `
+
+upstreamOwner: googleapis
+upstreamRepo: google-cloud-go
+title: chore: release 0.123.5
+branch: release-v0.123.5
+description: :robot: I have created a release \\*beep\\* \\*boop\\*
+---
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/v0.123.4...v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([1f9663c](https://www.github.com/googleapis/google-cloud-go/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([fcd1c89](https://www.github.com/googleapis/google-cloud-go/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
+primary: main
+force: true
+fork: false
+message: chore: release 0.123.5
+logger: [object Object]
+`
+
+exports['GoYoshi run root package filters special commits by scope: changes'] = `
+
+filename: CHANGES.md
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/v0.123.4...v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **core:** some core fix ([1f9663c](https://www.github.com/googleapis/google-cloud-go/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+
+`
+
+exports['GoYoshi run root package filters special commits by scope: options'] = `
+
+upstreamOwner: googleapis
+upstreamRepo: google-cloud-go
+title: chore: release 0.123.5
+branch: release-v0.123.5
+description: :robot: I have created a release \\*beep\\* \\*boop\\*
+---
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/v0.123.4...v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **core:** some core fix ([1f9663c](https://www.github.com/googleapis/google-cloud-go/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
+primary: main
+force: true
+fork: false
+message: chore: release 0.123.5
+logger: [object Object]
+`
+
+exports['GoYoshi run root package should ignore Release-As for scoped commits: changes'] = `
+
+filename: CHANGES.md
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/v0.123.4...v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **core:** some core fix ([1f9663c](https://www.github.com/googleapis/google-cloud-go/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+
+`
+
+exports['GoYoshi run root package should ignore Release-As for scoped commits: options'] = `
+
+upstreamOwner: googleapis
+upstreamRepo: google-cloud-go
+title: chore: release 0.123.5
+branch: release-v0.123.5
+description: :robot: I have created a release \\*beep\\* \\*boop\\*
+---
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/v0.123.4...v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **core:** some core fix ([1f9663c](https://www.github.com/googleapis/google-cloud-go/commit/1f9663cf08ab1cf3b68d95dee4dc99b7c4aac373))
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
+primary: main
+force: true
+fork: false
+message: chore: release 0.123.5
+logger: [object Object]
+`
+
+exports['GoYoshi run subpackage filters subpackage commits by scope: changes'] = `
+
+filename: CHANGES.md
+# Changelog
+
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/storage/vv0.123.4...storage/v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **storage:** some storage fix ([fcd1c89](https://www.github.com/googleapis/google-cloud-go/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+`
+
+exports['GoYoshi run subpackage filters subpackage commits by scope: options'] = `
+
+upstreamOwner: googleapis
+upstreamRepo: google-cloud-go
+title: chore: release storage 0.123.5
+branch: release-storage-v0.123.5
+description: :robot: I have created a release \\*beep\\* \\*boop\\*
+---
+### [0.123.5](https://www.github.com/googleapis/google-cloud-go/compare/storage/vv0.123.4...storage/v0.123.5) (1983-10-10)
+
+
+### Bug Fixes
+
+* **storage:** some storage fix ([fcd1c89](https://www.github.com/googleapis/google-cloud-go/commit/fcd1c890dc1526f4d62ceedad561f498195c8939))
+
+This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
+primary: main
+force: true
+fork: false
+message: chore: release storage 0.123.5
+logger: [object Object]
+`
