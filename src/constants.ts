8ee17edd1365eeffaf2029f58d809ada38d0a2d0
Benjamin E. Coe <bencoe@google.com>
2021-09-14 14:10:57 -0400

    Update src/constants.ts
    Co-authored-by: Jeff Ching <chingor@google.com>

--- src/constants.ts
+++ src/constants.ts
@@ -17,4 +17,4 @@ export const GH_API_URL = 'https://api.github.com';
 export const RELEASE_PLEASE = 'release-please';
 export const RELEASE_PLEASE_CONFIG = `${RELEASE_PLEASE}-config.json`;
 export const RELEASE_PLEASE_MANIFEST = `.${RELEASE_PLEASE}-manifest.json`;
-export const MAX_ISSUE_SIZE = 65536;
+export const MAX_ISSUE_BODY_SIZE = 65536;
