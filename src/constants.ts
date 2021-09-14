a9652442b636a146bf92b9be62334ae4ce33dd24
bcoe <bencoe@google.com>
2021-09-14 10:59:26 -0700

    fix: limit PR description to 65536 characters

--- src/constants.ts
+++ src/constants.ts
@@ -17,3 +17,4 @@ export const GH_API_URL = 'https://api.github.com';
 export const RELEASE_PLEASE = 'release-please';
 export const RELEASE_PLEASE_CONFIG = `${RELEASE_PLEASE}-config.json`;
 export const RELEASE_PLEASE_MANIFEST = `.${RELEASE_PLEASE}-manifest.json`;
+export const MAX_ISSUE_SIZE = 65536;
