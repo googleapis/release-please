e1e19d225123d3fc36b22c31520c4e64fe1d18ce
HelloCore <aukwat@gmail.com>
2021-09-14 18:09:50 +0700

    feat: add pubspec yaml updater

---
+++ __snapshots__/pubspec-yaml.js
@@ -0,0 +1,11 @@
+exports['PubspecYaml updateContent updates version in pubspec.yaml file 1'] = `
+name: hello_world
+description: Hello World
+publish_to: 'none' # Remove this line if you wish to publish to pub.dev
+
+version: 0.6.0+12
+
+environment:
+  sdk: '>=2.12.0 <3.0.0'
+
+`
