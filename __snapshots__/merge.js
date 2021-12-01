c12637613920eebbd8e177442e187a94b6d88864
Jeff Ching <chingor@google.com>
2021-12-01 09:07:35 -0800

    fix: combined manifest PR should include labels

--- __snapshots__/merge.js
+++ __snapshots__/merge.js
@@ -1,3 +1,22 @@
+exports['Merge plugin run merges all labels for pull requests 1'] = `
+:robot: I have created a release *beep* *boop*
+---
+
+
+<details><summary>python-pkg: 1.0.0</summary>
+
+Release notes for path: python, releaseType: python
+</details>
+
+<details><summary>@here/pkgA: 3.3.4</summary>
+
+Release notes for path: node, releaseType: node
+</details>
+
+---
+This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
+`
+
 exports['Merge plugin run merges multiple pull requests as a draft 1'] = `
 :robot: I have created a release *beep* *boop*
 ---
