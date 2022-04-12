33b8ce42667daf8db5bfd4ecc5ca25f38dcffa81
Matan Kushner <hello@matchai.dev>
2022-04-12 16:20:21 -0300

    Remove redundant tests

--- __snapshots__/rust.js
+++ __snapshots__/rust.js
@@ -1,21 +1,4 @@
-exports['Rust Crate buildReleasePullRequest detects a default component 1'] = `
-:robot: I have created a release *beep* *boop*
----
-
-
-### [0.123.5](https://github.com/googleapis/rust-test-repo/compare/rust-test-repo-v0.123.4...rust_test_repo-v0.123.5) (1983-10-10)
-
-
-### Bug Fixes
-
-* **deps:** update dependency com.google.cloud:google-cloud-spanner to v1.50.0 ([08ca011](https://github.com/googleapis/rust-test-repo/commit/08ca01180a91c0a1ba8992b491db9212))
-* **deps:** update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([845db13](https://github.com/googleapis/rust-test-repo/commit/845db1381b3d5d20151cad2588f85feb))
-
----
-This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
-`
-
-exports['Rust Workspace buildReleasePullRequest detects a default component 1'] = `
+exports['Rust buildReleasePullRequest detects a default component 1'] = `
 :robot: I have created a release *beep* *boop*
 ---
 
