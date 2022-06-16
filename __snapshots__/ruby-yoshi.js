42e9d3804c75066814e4f51be3fe85bfd5ec8990
Neha Bajaj <bajajnehaa@google.com>
2022-06-16 14:31:08 +0530

    fix failing test case

--- __snapshots__/ruby-yoshi.js
+++ __snapshots__/ruby-yoshi.js
@@ -8,7 +8,7 @@ exports['RubyYoshi buildReleasePullRequest returns release PR changes with semve
 #### Bug Fixes
 
 * update dependency com.google.cloud:google-cloud-spanner to v1.50.0
-* update dependency com.google.cloud:google-cloud-storage to v1.120.0
+* update dependency com.google.cloud:google-cloud-storage to v1.120.0 ([#1234](https://github.com/googleapis/ruby-test-repo/issues/1234))
 
 ---
 This PR was generated with [Release Please](https://github.com/googleapis/release-please). See [documentation](https://github.com/googleapis/release-please#release-please).
