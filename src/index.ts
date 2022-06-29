85549158152a6dc1551935b3e6f30952aef091c1
Jeff Ching <chingor@google.com>
2022-06-29 11:40:11 -0700

    feat: add manifest schemas

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {resolve} from 'path';
+
 export * as Errors from './errors';
 export {
   Manifest,
@@ -54,3 +56,5 @@ export {
 } from './changelog-notes';
 export {Logger, setLogger} from './util/logger';
 export {GitHub} from './github';
+export const configSchema = require(resolve('./src/schemas/config'));
+export const manifestSchema = require(resolve('./src/schemas/manifest'));
