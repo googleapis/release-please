0ecbfb7798896c225a3aaca50a7ec0202bbddb4c
Tim Schwenke <tim.schwenke@trallnag.com>
2022-07-12 22:44:48 +0200

    fix(python): Make __init__.py bump underscore / hyphen agnostic

--- src/strategies/python.ts
+++ src/strategies/python.ts
@@ -103,20 +103,20 @@ export class Python extends BaseStrategy {
     if (!projectName) {
       logger.warn('No project/component found.');
     } else {
-      updates.push({
-        path: this.addPath(`${projectName}/__init__.py`),
-        createIfMissing: false,
-        updater: new PythonFileWithVersion({
-          version,
-        }),
-      });
-      updates.push({
-        path: this.addPath(`src/${projectName}/__init__.py`),
-        createIfMissing: false,
-        updater: new PythonFileWithVersion({
-          version,
-        }),
-      });
+      [
+        `${projectName.replace('-', '_')}/__init__.py`,
+        `${projectName}/__init__.py`,
+        `src/${projectName.replace('-', '_')}/__init__.py`,
+        `src/${projectName}/__init__.py`,
+      ].forEach(path =>
+        updates.push({
+          path: this.addPath(path),
+          createIfMissing: false,
+          updater: new PythonFileWithVersion({
+            version,
+          }),
+        })
+      );
     }
 
     // There should be only one version.py, but foreach in case that is incorrect
