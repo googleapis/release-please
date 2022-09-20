db51b295d711e657ee5928bc32a3dbb88c793969
Jason Kölker <jason@koelker.net>
2022-09-20 15:51:51 -0500

    fix(commit-split): detect overlapping prefixes (#1638)
    Previously two prefixes were considered overlapping if one was contained
    in the other. For example `/config/` and `/aws/config` would fail even
    though the prefix is unique.
    
    Fixes: #1637
    
    Co-authored-by: Jeff Ching <chingor@google.com>

--- test/util/commit-split.ts
+++ test/util/commit-split.ts
@@ -34,10 +34,27 @@ describe('CommitSplit', () => {
       message: 'empty files',
       files: [],
     },
+    {
+      sha: 'hij',
+      message: 'path prefixes',
+      files: ['pkg5/foo.txt', 'pkg6/pkg5/foo.txt'],
+    },
   ];
   it('defaults to excluding empty commits', () => {
     const commitSplit = new CommitSplit();
     expect(commitSplit.includeEmpty).to.be.false;
+  });
+  it('uses path prefixes', () => {
+    const commitSplit = new CommitSplit({
+      packagePaths: ['pkg5', 'pkg6/pkg5'],
+    });
+    const splitCommits = commitSplit.split(commits);
+    expect(splitCommits['pkg1']).to.be.undefined;
+    expect(splitCommits['pkg2']).to.be.undefined;
+    expect(splitCommits['pkg3']).to.be.undefined;
+    expect(splitCommits['pkg4']).to.be.undefined;
+    expect(splitCommits['pkg5']).lengthOf(1);
+    expect(splitCommits['pkg6/pkg5']).lengthOf(1);
   });
   describe('including empty commits', () => {
     it('should separate commits', () => {
