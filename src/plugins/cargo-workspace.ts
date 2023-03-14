0303e2eb9fb082bd9657baa117961d8567eba7e6
Jeff Ching <chingor@google.com>
2023-03-14 12:58:37 -0700

    fix(cargo-workspace): validate Cargo.toml version field (#1877)
    * test: failing test for invalid version field in Cargo.toml
    
    * fix(cargo-workspace): throw ConfigurationError if Cargo.toml has invalid version entry
    
    ---------
    
    Co-authored-by: Benjamin E. Coe <bencoe@google.com>

--- src/plugins/cargo-workspace.ts
+++ src/plugins/cargo-workspace.ts
@@ -37,6 +37,7 @@ import {PullRequestBody} from '../util/pull-request-body';
 import {BranchName} from '../util/branch-name';
 import {PatchVersionUpdate} from '../versioning-strategy';
 import {CargoLock} from '../updaters/rust/cargo-lock';
+import {ConfigurationError} from '../errors';
 
 interface CrateInfo {
   /**
@@ -137,8 +138,16 @@ export class CargoWorkspace extends WorkspacePlugin<CrateInfo> {
 
       const version = manifest.package?.version;
       if (!version) {
-        throw new Error(
-          `package manifest at ${manifestPath} is missing [package.version]`
+        throw new ConfigurationError(
+          `package manifest at ${manifestPath} is missing [package.version]`,
+          'cargo-workspace',
+          `${this.github.repository.owner}/${this.github.repository.repo}`
+        );
+      } else if (typeof version !== 'string') {
+        throw new ConfigurationError(
+          `package manifest at ${manifestPath} has an invalid [package.version]`,
+          'cargo-workspace',
+          `${this.github.repository.owner}/${this.github.repository.repo}`
         );
       }
       allCrates.push({
