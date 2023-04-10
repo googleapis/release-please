250e8418177c28c0d0da757a1fec519724f80eed
Jeff Ching <chingor@google.com>
2023-04-10 12:32:40 -0700

    build: update to latest cloud-rad doc generator

--- .kokoro/release/docs-devsite.sh
+++ .kokoro/release/docs-devsite.sh
@@ -25,5 +25,5 @@ if [[ -z "$CREDENTIALS" ]]; then
 fi
 
 npm install
-npm install --no-save @google-cloud/cloud-rad@^0.2.5
+npm install --no-save @google-cloud/cloud-rad@^0.3.5
 npx @google-cloud/cloud-rad
