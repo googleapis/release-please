9917dabed4be170800a135b0aecf588b62fff7a9
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2025-02-03 21:48:46 +0100

    fix: fix the go workspace version matching

--- __snapshots__/go-workspace.js
+++ __snapshots__/go-workspace.js
@@ -22,7 +22,7 @@ Release notes for path: packages/goA, releaseType: go
 ### Dependencies
 
 * The following workspace dependencies were updated
-  * example.com/packages/goB/v2 bumped from 2.2.2 to 2.2.3
+  * example.com/packages/goB/v2 bumped from 2.2.2-0.20250203122516-4c838e530ecb to 2.2.3
 </details>
 
 <details><summary>example.com/packages/goE: 3.3.4</summary>
@@ -66,7 +66,7 @@ Release notes for path: packages/goB, releaseType: go
 ### Dependencies
 
 * The following workspace dependencies were updated
-  * example.com/packages/goB/v2 bumped from 2.2.2 to 2.3.0
+  * example.com/packages/goB/v2 bumped from 2.2.2-0.20250203122516-4c838e530ecb to 2.3.0
 </details>
 
 ---
@@ -96,7 +96,7 @@ Release notes for path: packages/goA, releaseType: go
 ### Dependencies
 
 * The following workspace dependencies were updated
-  * example.com/packages/goB/v2 bumped from 2.2.2 to 2.2.3
+  * example.com/packages/goB/v2 bumped from 2.2.2-0.20250203122516-4c838e530ecb to 2.2.3
 </details>
 
 <details><summary>example.com/packages/goD: 4.4.5</summary>
@@ -139,7 +139,7 @@ Release notes for path: packages/goA, releaseType: go
 ### Dependencies
 
 * The following workspace dependencies were updated
-  * example.com/packages/goB/v2 bumped from 2.2.2 to 2.2.3
+  * example.com/packages/goB/v2 bumped from 2.2.2-0.20250203122516-4c838e530ecb to 2.2.3
 </details>
 
 <details><summary>example.com/packages/goD: 4.4.5</summary>
--- test/fixtures/plugins/go-workspace/packages/goC/go.mod
+++ test/fixtures/plugins/go-workspace/packages/goC/go.mod
@@ -3,5 +3,5 @@ module example.com/packages/goC
 go 1.23.0
 
 require (
-	example.com/packages/goB/v2 v2.2.2
+	example.com/packages/goB/v2 v2.2.2-0.20250203122516-4c838e530ecb // indirect
 )
